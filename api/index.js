4fb7c88d8c28071224708b537258e2e1753dd5b0
Marcos M. Crocci <marcoscrocci@gmail.com>
2022-09-14 23:57:11 -0300

    feat(api) Incluindo turma

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
-const { ApolloServer } = require('apollo-server');
-const userScheme = require('./user/schema/user.graphql');
-const userResolvers = require('./user/resolvers/userResolvers');
-const UsersAPI = require('./user/datasource/user');
+const { ApolloServer } = require('apollo-server')
+const { mergeTypeDefs } = require('@graphql-tools/merge');
+const path = require('path')
 
-const typeDefs = [userScheme]
-const resolvers = [userResolvers]
+const userSchema = require('./user/schema/user.graphql')
+const userResolvers = require('./user/resolvers/userResolvers')
+const UsersAPI = require('./user/datasource/user')
+
+const turmaSchema = require('./turma/schema/turma.graphql')
+const turmaResolvers = require('./turma/resolvers/turmaResolvers')
+//const TurmasAPI = require('./turma/datasource/turma')
+
+const typeDefs = mergeTypeDefs([userSchema, turmaSchema])
+
+// https://cursos.alura.com.br/forum/topico-typeerror-mergetypedefs-is-not-a-function-245972
+const resolvers = [userResolvers, turmaResolvers]
 
 const server = new ApolloServer({ 
     typeDefs, 
