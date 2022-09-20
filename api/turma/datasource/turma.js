868d4cd34711f546ffe74f6d711315a88b0f8499
Marcos M. Crocci <marcoscrocci@gmail.com>
2022-09-20 20:27:35 -0300

    feat(SQLite3) CRUD de Turmas

--- api/turma/datasource/turma.js
+++ api/turma/datasource/turma.js
@@ -1,14 +1,53 @@
-const { SQLDataSource } = require('datasource-sql');
+const { SQLDataSource } = require("datasource-sql");
 
 class TurmasAPI extends SQLDataSource {
     constructor(dbConfig) {
         super(dbConfig);
+        this.responseCustom = {
+            code: 0,
+            message: ""
+        };
     }
 
     async getTurmas() {
-        return this.db
-            .select('*')
-            .from('turmas');
+        return await this.db.select("*").from("turmas");
+    }
+
+    async getTurma(id) {
+        const turma = await this.db
+            .select("*")
+            .from("turmas")
+            .where({ id: Number(id) });
+        return turma[0];
+    }
+
+    async addTurma(novaTurma) {
+        const novaTurmaId = await this.db
+            .insert(novaTurma)
+            .returning("id")
+            .into("turmas");
+
+        const turmaInserida = await this.getTurma(novaTurmaId[0].id);
+        return turmaInserida;
+    }
+
+    async updateTurma(novosDados) {
+        await this.db
+            .update({ ...novosDados.turma })
+            .where({ id: Number(novosDados.id) })
+            .into("turmas");
+
+        const turmaAtualizada = await this.getTurma(novosDados.id);
+        return {
+            ...turmaAtualizada
+        };
+    }
+
+    async deleteTurma(id) {
+        await this.db("turmas").where({ id: id }).del();
+
+        this.responseCustom.message = "Registro excluído!";
+        return this.responseCustom;
     }
 }
 
--- api/turma/resolvers/turmaResolvers.js
+++ api/turma/resolvers/turmaResolvers.js
@@ -1,7 +1,29 @@
-const turmaResolvers = {
-    Query: {
-        turmas: (parent, args, { dataSources }) => dataSources.turmasAPI.getTurmas()
-    }
-}
+const { GraphQLScalarType } = require("graphql");
 
-module.exports = turmaResolvers
+const turmaResolvers = {
+    DateTime: new GraphQLScalarType({
+        name: "DateTime",
+        description: "string de data e hora no formato ISO-8601",
+        serialize: value => new Date(value).toISOString(),
+        parseValue: value => new Date(value),
+        parseLiteral: ast => new Date(ast.value).toISOString()
+    }),
+
+    Query: {
+        turmas: (parent, args, { dataSources }) =>
+            dataSources.turmasAPI.getTurmas(),
+        turma: (parent, { id }, { dataSources }) =>
+            dataSources.turmasAPI.getTurma(id)
+    },
+
+    Mutation: {
+        addTurma: (parent, { turma }, { dataSources }) =>
+            dataSources.turmasAPI.addTurma(turma),
+        updateTurma: (parent, novosDados, { dataSources }) =>
+            dataSources.turmasAPI.updateTurma(novosDados),
+        deleteTurma: (parent, { id }, { dataSources }) =>
+            dataSources.turmasAPI.deleteTurma(id)
+    }
+};
+
+module.exports = turmaResolvers;
